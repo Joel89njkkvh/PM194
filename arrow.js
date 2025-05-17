bedd3d1c4cea212317c99d855524934e8e20b982
Joel <151809158+Joel89njkkvh@users.noreply.github.com>
2025-05-16 20:03:11 -0600

    Subiendo archivos JS de práctica 1

---
+++ arrow.js
@@ -0,0 +1,8 @@
+const cuadrado = (numero) => numero * numero;
+
+console.log(cuadrado(1)); 
+console.log(cuadrado(2)); 
+console.log(cuadrado(21)); 
+const saludoPersonalizado = (nombre, edad) => `Hola, me llamo ${nombre} y tengo ${edad} años.`;
+
+console.log(saludoPersonalizado("Joel", 20)); 
---
+++ variables.js
@@ -0,0 +1,6 @@
+const nombre = "Armando"; 
+let edad = 25; 
+
+const saludo = `Hola, ${nombre}. Tienes ${edad} años.`; 
+
+console.log(saludo);
