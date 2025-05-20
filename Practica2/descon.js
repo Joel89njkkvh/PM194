6c7aa12ad101b3417a2ffedcae90b7228d3785b9
Joel <151809158+Joel89njkkvh@users.noreply.github.com>
2025-05-20 16:41:03 -0600

    Subiendo práctica 1 y práctica 2

---
+++ Practica2/descon.js
@@ -0,0 +1,12 @@
+const persona = {
+  nombre: "Joel",
+  edad: 37,
+  direccion: {
+    ciudad: "Qro",
+    pais: "MX"
+  }
+};
+
+const { nombre, edad, direccion: { ciudad } } = persona;
+
+console.log(`Me llamo ${nombre}, tengo ${edad} años y vivo en ${ciudad}.`);
