b602b0168eb0ed0b55fb670b986060670290d95d
Joel <151809158+Joel89njkkvh@users.noreply.github.com>
2025-06-11 19:17:38 -0600

    react

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,33 +1,38 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+//ZONA 1----Importaciones
 
-const Texto = ({ contenido }) => {
-  return <Text style={styles.text}>{contenido}</Text>;
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button} from 'react-native';
+import React,{useState} from 'react';
+
+const Texto=(props) => {
+  const {children}=props
+  return(
+  <Text>{children}</Text>
+  )
 };
 
+
+//ZONA 2----Main(Ejecucion)
 export default function App() {
   return (
     <View style={styles.container}>
-      <Texto contenido="hola mundo react native joel" />
-      <BotonPersonalizado titulo="presionar" accion={() => alert('¡Botón presionado!')} />
-      <StatusBar style="auto" />
+      <Button title="Tlajaba!!"></Button>
+      <Texto >Hola</Texto>
+      <Texto >Mundo</Texto>
+      <Texto >React Native</Texto>
+
+      <StatusBar style="auto" />  
     </View>
   );
 }
 
-const BotonPersonalizado = ({ titulo, accion }) => {
-  return <Button title={titulo} onPress={accion} />;
-};
 
+//ZONA 3----Estilos, estetica del screen
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  text: {
-    fontSize: 24,
-    marginBottom: 20,
   },
 });
