c602ec8963e9650579c6988bf831a49275d17afa
Joel <151809158+Joel89njkkvh@users.noreply.github.com>
2025-06-11 20:00:23 -0600

    tlabaja
    deja de tlabajal

--- my-blank/App.js
+++ my-blank/App.js
@@ -2,24 +2,34 @@
 
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button} from 'react-native';
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 
-const Texto=(props) => {
-  const {children}=props
-  return(
-  <Text>{children}</Text>
-  )
+const Texto = (props) => {
+  const [contenido, setContenido] = useState("Hola Mundo RNative");
+  const actualizarTexto = () => { setContenido('Estado actualizado del Text') };
+  return (
+    <Text onPress={actualizarTexto}>{contenido}</Text>
+  );
+};
+
+const Boton = () => {
+  const [boton, setBoton] = useState("Tlajaba!!");
+  const cambiarBoton = () => { setBoton("Deja Tlajaba!!") };
+  return (
+    <Button title={boton} onPress={cambiarBoton} />
+  );
 };
 
 
 //ZONA 2----Main(Ejecucion)
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Button title="Tlajaba!!"></Button>
-      <Texto >Hola</Texto>
-      <Texto >Mundo</Texto>
-      <Texto >React Native</Texto>
+      <Boton />
+      <Texto > </Texto>
+      <Texto > </Texto>
+      <Texto > </Texto>
 
       <StatusBar style="auto" />  
     </View>
