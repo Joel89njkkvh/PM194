dde14389ac9b7efdd5645ad8e46fe4da21b6ddbd
Joel <151809158+Joel89njkkvh@users.noreply.github.com>
2025-06-13 20:12:32 -0600

    styles
    13/06/2025

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,14 +1,14 @@
-//ZONA 1----Importaciones
-
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button} from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import React, { useState } from 'react';
 
-const Texto = (props) => {
+const Texto = ({ style }) => {
   const [contenido, setContenido] = useState("Hola Mundo RNative");
   const actualizarTexto = () => { setContenido('Estado actualizado del Text') };
   return (
-    <Text onPress={actualizarTexto}>{contenido}</Text>
+    <Text style={[styles.text, style]} onPress={actualizarTexto}>
+      {contenido}
+    </Text>
   );
 };
 
@@ -20,29 +20,34 @@ const Boton = () => {
   );
 };
 
-
-//ZONA 2----Main(Ejecucion)
-
 export default function App() {
   return (
     <View style={styles.container}>
-      <Boton />
-      <Texto > </Texto>
-      <Texto > </Texto>
-      <Texto > </Texto>
-
+      {/* <Boton /> */}
+      <Texto     style={styles.red} />
+      <Texto     style={styles.blue} />
+      <Texto     style={styles.green} />
       <StatusBar style="auto" />  
     </View>
   );
 }
 
-
-//ZONA 3----Estilos, estetica del screen
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems:      'baseline',
+    justifyContent:  'center', 
+    flexDirection:   'row-reverse',
   },
+  text: {
+    color: "white",
+    fontSize: 20,
+    // padding:  10,
+    // margin:   10,
+    // textAlign: 'center',
+  },
+  red:   { backgroundColor: 'red'},
+  blue:  { backgroundColor: 'blue' ,},
+  green: { backgroundColor: 'green',},
 });
