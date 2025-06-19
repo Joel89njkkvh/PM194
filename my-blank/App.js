d79c03f24c36f37f1f2037237ccc7c2a2f7eb7bc
Joel <151809158+Joel89njkkvh@users.noreply.github.com>
2025-06-18 20:05:23 -0600

    interruptor

--- my-blank/App.js
+++ my-blank/App.js
@@ -2,28 +2,48 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, Switch } from 'react-native';
 import React, { useState } from 'react';
 
-const Texto = ({ style }) => {
-  const [contenido, setContenido] = useState("Hola Mundo RNative");
-  const actualizarTexto = () => { setContenido('Estado actualizado del Text') };
+const Interruptor = () => {
+  const [isEnabled, setIsEnabled] = useState(false);
+
+  const toggleSwitch = () => {
+    setIsEnabled(previousState => !previousState);
+  };
+
   return (
-    <Text style={[styles.text, style]} onPress={actualizarTexto}>
-      <View style={{margin: 10 }}>
-        <Text style={[styles.text, style]}>{contenido}</Text>
-        <Button title='actualizarTexto'onPress={actualizarTexto} color='purple'></Button>
-      </View>
-    </Text>
+    <View style={styles.container}>
+      <Text>{isEnabled ? 'Activado' : 'Desactivado'}</Text>
+
+      <Switch
+        trackColor={{ false: "#767577", true: "#81b0ff" }}
+        thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
+    </View>
   );
 };
 
+const Texto = ({ style }) => {
+  const [contenido, setContenido] = useState("Hola Mundo RNative");
+  const actualizarTexto = () => { setContenido('Estado actualizado del Text') };
+
+  return (
+    <View style={{ margin: 10 }}>
+      <Text style={[styles.text, style]}>{contenido}</Text>
+      <Button title='Actualizar Texto' onPress={actualizarTexto} color='purple' />
+    </View>
+  );
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-    
-      <Texto     style={styles.red} />
-      <Texto     style={styles.blue} />
-      <Texto     style={styles.green} />
-      <StatusBar style="auto" />  
+      <Interruptor />
+      <Texto style={styles.red} />
+      <Texto style={styles.blue} />
+      <Texto style={styles.green} />
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -32,18 +52,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems:      'baseline',
-    justifyContent:  'center', 
-    flexDirection:   'row-reverse',
+    alignItems: 'baseline',
+    justifyContent: 'center',
+    flexDirection: 'row-reverse',
   },
   text: {
     color: "white",
     fontSize: 20,
-    // padding:  10,
-    // margin:   10,
-    // textAlign: 'center',
   },
-  red:   { backgroundColor: 'red'},
-  blue:  { backgroundColor: 'blue' ,},
-  green: { backgroundColor: 'green',},
+  red: { backgroundColor: 'red' },
+  blue: { backgroundColor: 'blue' },
+  green: { backgroundColor: 'green' },
 });
